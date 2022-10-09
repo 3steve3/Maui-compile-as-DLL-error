8b0bdb1da0cc7260690ddbe079c04a0fe487a681
Steve <stna7972@colorado.edu>
2022-10-09 16:17:00 -0600

    Add project files.

---
+++ CompileAsDLLError/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CompileAsDLLError;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CompileAsDLLError/Platforms/Android/MainApplication.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace CompileAsDLLError;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
